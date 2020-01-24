e7b0fd571bc6c59d2bf339937e88f8ffbc302a64
ed <ed@elirandedi.com>
2020-01-24 18:29:16 +0000

    dat: added keyfn lut for keyboard input

--- dat.h
+++ dat.h
@@ -1,9 +1,32 @@
+#define scrrect	screen->r
 #define	scrmax	screen->r.max
 #define	scrmin	screen->r.min
 #define scrmaxx	scrmax.x
 #define scrmaxy	scrmax.y
 #define scrminx scrmin.x
 #define scrminy	scrmin.y
+
+typedef void Fn(void);
+typedef struct	Keyfn	Keyfn;
+typedef union	Color	Color;
+struct Keyfn {
+	Rune r;
+	//void(*f)(void);
+	Fn *f;
+};
+union Color {
+	ulong u;
+	Image *i;
+};
+
+
+void quit(void);
+void unused(void);
+void redraw(void);
+void scrolldown(void);
+void scrollup(void);
+void search(void);
+void mark(void);
 
 Biobuf	*bio;
 char	*stack[8192*4];
@@ -20,6 +43,7 @@ char	kbuf[256];
 char	*kbpos = kbuf;
 char	*manpages[16];
 char	*grepstrings[256];
+int	*kbd = &e.kbdc;
 int	grepfound[256];
 int	*grepf = grepfound;
 int	nfound;
@@ -37,14 +61,28 @@ Image	*red;
 Image	*yellow;
 Image	*eprompt, *sprompt, *fprompt;
 Image	*gray;
-Image	*bigscreen;
 Image	*undo;
 Image	*oneline;
+Image	*bigscreen;
 Rectangle	bigrect;
 Point	bigpoint;
+Point	cmd;
 
 enum{
 	Rioscroll = 26,
+};
+
+Keyfn kbdfn[]={
+	Kdown,	scrolldown,
+	Kup,	scrollup,
+	Kleft,	unused,
+	Kright,	unused,
+	'q',	quit,
+	Kesc,	quit,
+	Kdel,	quit,
+	'/',	search,
+	'r',	redraw,
+	'm',	mark,
 };
 
 char	*Search = "look for";
